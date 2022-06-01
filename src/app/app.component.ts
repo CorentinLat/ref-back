ff0e7fa9e7f07a5125cc0d719bb61928538d5a86
Latappy Corentin <corentin.latappy@etu.u-bordeaux.fr>
2022-06-01 23:53:48 +0200

    Configuring app with Electron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ElectronService } from './core/services';
+import { TranslateService } from '@ngx-translate/core';
+import { APP_CONFIG } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'perf-arbitres-plus-plus';
+  constructor(
+    private electronService: ElectronService,
+    private translate: TranslateService
+  ) {
+    this.translate.setDefaultLang('fr');
+    console.log('APP_CONFIG', APP_CONFIG);
+
+    if (electronService.isElectron) {
+      console.log(process.env);
+      console.log('Run in electron');
+      console.log('Electron ipcRenderer', this.electronService.ipcRenderer);
+      console.log('NodeJS childProcess', this.electronService.childProcess);
+    } else {
+      console.log('Run in browser');
+    }
+  }
 }
