e136e952f7b54529b55fc1408b37ab3dec94e22c
Latappy Corentin <corentin.latappy@etu.u-bordeaux.fr>
2024-02-03 12:21:17 +0100

    💚 Trying to fix CI notarize

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -13,7 +13,7 @@ exports.default = async function notarizeMacos(context) {
         return;
     }
 
-    if (!('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env)) {
+    if (!('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env && 'APPLE_TEAM_ID' in process.env)) {
         console.warn('Skipping notarizing step. APPLE_ID and APPLE_ID_PASS env variables must be set');
         return;
     }
