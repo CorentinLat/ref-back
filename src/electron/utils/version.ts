5b62ea66cb9960b3db2ac1c552cbeee37d417094
Latappy Corentin <corentin.latappy@etu.u-bordeaux.fr>
2025-04-08 12:55:25 +0200

    🔖 4.2.2

--- src/electron/utils/version.ts
+++ src/electron/utils/version.ts
@@ -7,7 +7,7 @@ import translate from '../translation';
 
 type VersionFile = { lastVersion?: string };
 
-const VERSIONS = ['4.1.0', '4.1.1', '4.2.0', '4.2.1'];
+const VERSIONS = ['4.1.0', '4.1.1', '4.2.0', '4.2.1', '4.2.2'];
 
 export const checkNewVersionInstalled = () => {
     logger.info('Checking new version installed');
